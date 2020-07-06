32a115447526026796016c35e913daf00e74cb21
lpeng <lpeng@yealink.com>
2020-07-06 14:45:07 +0800

    【modified】1、Python脚本中添加valgrind和gprof功能；2、更新README。

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # autoCodecTest
 This is a codecs coherence and performance test script. 
 
-## 支持平台
+## 1、支持功能
+
+1、支持批量进行编码或解码；
+2、支持对编解码的性能进行数据统计；
+3、支持对编解码进行一致性验证;
+4、支持对待验证编解码器进行valgrind内存检查；
+5、支持gprof。 
+
+## 2、支持平台（platform/OS supported）
 Windows, Linux(ARM)
 
-## 使用方法
+## 3、使用方法
 
 ### 一、shell脚本
 ./auto_codec_test.sh ./bin/xvid_decraw ./stream  ./out 1 ./bin/xvid_decraw  0 0
 
 
 ### 二、Python脚本
+python auto_codec_test.py  ./bin/xvid_decraw  ./stream/ ./out/python  1 1 ./bin/xvid_decraw  0
+python auto_codec_test.py  ./bin/xvid_decraw  ./stream/ ./out/python  0 1 ./bin/xvid_decraw  1
 
-待补充
+## 4、注意事项
+
+1、valgrind和gpof不能同时使用。
 
--- auto_codec_test.py
+++ auto_codec_test.py
@@ -1,13 +1,13 @@
 #_*_ coding=UTF-8_*_  #脚本中有中文注释必须包含这一句
 
-##脚本用法： python auto_codec_coherence.py rawDemo srcStreamDir outFileDir [gprofflag outyuvflag refDemo]
+##脚本用法： python auto_codec_coherence.py rawDemo srcStreamDir outFileDir [gprofflag outyuvflag refDemo memcheckflag]
 ##参数说明：	rawDemo		:	待验证的可执行文件
 ##				srcStreamDir:	码流路径
 ##				outFileDir	:	结果输出路径
 ##				gprofflag	:	gprof开关(默认为0)：如果取0表示不会利用gprof工具进行分析;取1表示利用gprof进行分析
 ##				outyuvflag	:	yuv输出开关(默认为0)：如果取0，表示解码不输出yuv,否则相反。
 ##				refDemo		: 	参考可执行文件
-
+##				memcheckflag:	使用valgrind进行内存泄露检查(默认为0)(针对linux下编译的64位库)
 import os
 import re
 import sys
@@ -24,8 +24,8 @@ collect = '_pyout_collect'  		#性能对比输出文件夹
 cmp_match = '_pyout_match'  		#yuv对比一致
 cmp_dismatch = '_pyout_dismatch' 	# yuv对比不一致
 Anchor_Ndec = '_pyAnchor_notdecstream'	#待验证的可执行文件不能解码的码流
-Reference_Ndec = '_pyRef_notdecstream'		#参考可执行文件不能解码的码流
-Anchor_memchecklog = '_pyAnchor_memcheck' #对待验证可执行文件进行内存检在查后汇总文件
+Reference_Ndec = '_pyRef_notdecstream'	#参考可执行文件不能解码的码流
+Anchor_memchecklog = '_pyAnchor_memcheck' #对待验证可执行文件进行内存检查后的汇总文件
 
 #比较两个文件是否相同, 相同则返回True, 不同返回False
 def	yuv_cmp(file1,file2):
@@ -101,6 +101,28 @@ def get_data_from_txt(filename, txtfile, outdatafile, anchor='1'):
 				Data['fps '] + ' '*5 + '\n'     
         pFile.write(oneline)
         pFile.close()
+
+#linux下内存泄漏检查valgrind
+def perform_valgrind_data(outFileDir, Anchordeccmd='0', onlystreamname='0', Refdeccmd='0', reserve='0'):
+    outmemcheckanchortxt = outFileDir + delimiter  + '__pyMemcheckAnchor.log'
+    outmemcheckreftxt = outFileDir + delimiter  + '__pyMemcheckRef.log'
+    if Anchordeccmd != '0':
+        redirectcmd = Anchordeccmd 
+        cmd = ' '.join(['valgrind',
+                        '--log-file='+outmemcheckanchortxt,
+                        '--leak-check=yes --show-reachable=yes --track-origins=yes',
+                        redirectcmd])
+        print(cmd)
+        ret = subprocess.call(cmd, shell=True)
+    if Refdeccmd != '0':
+        redirectcmd = Refdeccmd
+        cmd = ' '.join(['valgrind',
+                '--log-file='+outmemcheckreftxt,
+                '--leak-check=yes --show-reachable=yes --track-origins=yes',
+                redirectcmd])
+        print(cmd)
+        ret = subprocess.call(cmd, shell=True)
+        return ret
         
 #从valgrind输出日志中获取数据	
 def get_data_from_log(filename, logfile, outdatafile):
@@ -108,10 +130,11 @@ def get_data_from_log(filename, logfile, outdatafile):
 	lines = pFile.readlines()
 	lineflag = 0
 	for i in range(len(lines)):
-		if lines[i].find('total heap usage') != 1:
-			infoline = lines[i]
+	    if lines[i].find('total heap usage') != -1:
+	        infoline = lines[i]
+	        print infoline
 	pFile.close()
-	#打开汇总文件ilename
+	#打开汇总文件filename
 	pFile = open(outdatafile, 'a+')
 	oneline = '[ ' + filename + ' ]: ' + infoline + '\n'
 	pFile.writelines(oneline)
@@ -119,7 +142,7 @@ def get_data_from_log(filename, logfile, outdatafile):
 	return
 	
 #编码或解码处理			
-def process_encode_decode(rawDemo, srcBinDir, outFileDir, gprof='0', yuvflag='0', refDemo='0'):
+def process_encode_decode(rawDemo, srcBinDir, outFileDir, gprof='0', yuvflag='0', refDemo='0', memcheckflag='0'):
 	if (os.path.exists(srcBinDir) == False):
 		print('the input file path is not exist')
 		return -1
@@ -133,7 +156,7 @@ def process_encode_decode(rawDemo, srcBinDir, outFileDir, gprof='0', yuvflag='0'
 	outanchorNdec = outFileDir + delimiter + Anchor_Ndec + '.txt'
 	outredNdec = outFileDir + delimiter + Reference_Ndec + '.txt'
 	outMemchecklog = outFileDir + delimiter + Anchor_memchecklog + '.log'
-	
+        
 	maxch = 70
 	spacesymbo = "-"
 	
@@ -154,43 +177,88 @@ def process_encode_decode(rawDemo, srcBinDir, outFileDir, gprof='0', yuvflag='0'
                 
 		space_num = maxch - len(filename)
 		onlystreamname = get_file_name(filename)
+		#print filename
+		#print onlystreamname
+		
 		outrawtxt = outFileDir + delimiter + onlystreamname + '_Anchordec.txt'
 		outreftxt = outFileDir + delimiter + onlystreamname + '_Refdec.txt'
 		outrawyuv = outFileDir + delimiter + onlystreamname + '_Anchordec.yuv'
 		outrefyuv = outFileDir + delimiter + onlystreamname + '_Refdec.yuv'
-
+                outmemcheckanchortxt = outFileDir + delimiter  + '__pyMemcheckAnchor.log'
+                outmemcheckreftxt = outFileDir + delimiter  + '__pyMemcheckRef.log'
+                
+                pFileAnchorNdec = open(outanchorNdec, 'w')
+                pFileRefNdec = open(outredNdec, 'w')
+                
 		# 原始可执行文件解码
-		cmd_raw = space.join([rawDemo, '-i', filename, '-c', 'i420', '-f', 'yuv', '-d', outrawyuv, '>', outrawtxt])
+		if yuvflag != '0':
+                    cmd_raw = space.join([rawDemo, '-i', filename, '-c', 'i420', '-f', 'yuv', '-d', outrawyuv, '>', outrawtxt])
+		else:
+                    cmd_raw = space.join([rawDemo, '-i', filename, '-c', 'i420', '-f', 'yuv', '>', outrawtxt])
 		ret = subprocess.call(cmd_raw, shell=True)
 		if(ret!=0):
-			print('rawDemo failed!')
-			return -1
+		    print(filename + ' rawDemo failed!')
+		    pFileAnchorNdec.write(rawDemo+'cannot dec'+filename+' '+' ret: '+ bytes(ret)+'\n')
+		    return -1
+	        else:
+                    print(filename + ' rawDemo success!')    
+		
 		# 参考可执行文件解码
 		if refDemo != '0':
-			cmd_ref = space.join([refDemo, '-i', filename, '-c', 'i420', '-f', 'yuv', '-d', outrefyuv, '>', outreftxt])
-			ret = subprocess.call(cmd_ref, shell=True)
-			if(ret!=0):
-				print('refDemo failed!')
-				return -1
-		
+                    if yuvflag != '0':
+                        cmd_ref = space.join([refDemo, '-i', filename, '-c', 'i420', '-f', 'yuv', '-d', outrefyuv, '>', outreftxt])
+                    else:
+                        cmd_ref = space.join([refDemo, '-i', filename, '-c', 'i420', '-f', 'yuv', '>', outreftxt])
+                    
+                    ret = subprocess.call(cmd_ref, shell=True)
+                    if(ret==0):
+		        print(filename + ' refDemo success!')
+		    else:
+                        print(filename + ' refDemo failed!')
+                        pFileRefNdec.write(refDemo+'cannot dec'+filename+' '+' ret: '+ bytes(ret)+'\n')
+                        return -1
+                
+                    ## 5. gprof性能分析
+		    if(int(gprof)==1): #默认为0，表示不使用性能分析工具gprof
+                        cmd = space.join(['gprof', rawDemo, 'gmon.out', '>', outFileDir+'/outgprof/'+onlystreamname+'_gprof_anchor.txt'])
+                        print(cmd)
+                        subprocess.call(cmd, shell=True)
+                        
+		    ## 6.valgrind内存检查
+		    if(memcheckflag != '0'):
+                        #print cmd_ref
+                        #exit()
+                        ret = perform_valgrind_data(outFileDir, cmd_raw, filename, cmd_ref)
+                        get_data_from_log(filename, outmemcheckanchortxt, outMemchecklog)
+                        
 		## 3. 一致性比较
-		ret = yuv_cmp(outrawyuv, outrefyuv)
-		if(ret!=0):
-                    print('MATCH!')
-                    coherence= '[' + filename + ']:' + space + 'MATCH!'
-                    pFileMatch.write(coherence)
-                    pFileMatch.write('\n')
-                    pFileMatch.close()
-                else:
-                    print('DISMATCH!')
-                    coherence= '[' + filename + ']:' + space + 'DISMATCH!'
-                    pFileDismatch.write(coherence)
-                    pFileDismatch.write('\n')
-                    pFileDismatch.close()
+                if(refDemo != '0' and int(yuvflag) != 0):
+		    ret = yuv_cmp(outrawyuv, outrefyuv)
+		    if(ret!=0):
+                        print('MATCH!')
+                        coherence= '[' + filename + ']:' + space + 'MATCH!'
+                        pFileMatch.write(coherence)
+                        pFileMatch.write('\n')
+                        os.remove(outrawyuv)
+                        os.remove(outrefyuv)
+                    else:
+                        print('DISMATCH!')
+                        coherence= '[' + filename + ']:' + space + 'DISMATCH!'
+                        pFileDismatch.write(coherence)
+                        pFileDismatch.write('\n')
+                        
                 ## 4.将性能数据结果输出到excel中
-		get_data_from_txt(filename, outrawtxt, outtotal, 1)
-                get_data_from_txt(filename, outreftxt, outtotal, 0)
+		    get_data_from_txt(filename, outrawtxt, outtotal, 1)
+		    if(refDemo != '0'):
+                        get_data_from_txt(filename, outreftxt, outtotal, 0)
 
+                ## 关闭打开的文件               
+                    pFileAnchorNdec.close()
+                    if refDemo !=0:
+                        pFileMatch.close()
+                        pFileDismatch.close()
+                        pFileRefNdec.close()
+                                    
 
 ####################################main 函数入口####################################################
 if __name__ == '__main__':
@@ -202,19 +270,22 @@ if __name__ == '__main__':
     srcStreamDir = sys.argv[2]
     outFileDir = sys.argv[3]
 	
-    if len(sys.argv) == 5:
+    if len(sys.argv) >= 5:
         gprof = sys.argv[4]  
     else: 
 	gprof = 0
-    if len(sys.argv) == 6:
+    if len(sys.argv) >= 6:
 	yuvflag = sys.argv[5]
     else:
 	yuvflag = 0
-    if len(sys.argv) == 7:	
+    if len(sys.argv) >= 7:	
 	refDemo = sys.argv[6]
     else:
 	refDemo = 0
-
-    ret = process_encode_decode(rawDemo, srcStreamDir, outFileDir, gprof, yuvflag, refDemo)
+    if len(sys.argv) >= 8:
+	memcheckflag = sys.argv[7]
+    else:
+	memcheckflag = 0
+    ret = process_encode_decode(rawDemo, srcStreamDir, outFileDir, gprof, yuvflag, refDemo, memcheckflag)
     if (ret!=0):
 	exit()
